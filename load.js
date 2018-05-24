5665b35122c3b103dcc9a25330f20d874dea8f0e
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-05-24 18:53:17 +0900

    some fix

--- load.js
+++ load.js
@@ -1,10 +1,8 @@
 (function() {
   var date = new Date();
   time = date.getTime();
-
   if (! document.getElementById('tmp-style')) {
     var style = document.createElement('link');
-    style.href = "https://127.0.0.1:8080/build/style.css?" + time;
     style.rel = "stylesheet";
     style.type = "text/css";
     style.id = "tmp-style";
@@ -13,8 +11,10 @@
 
   if (!document.getElementById('tmp-script')) {
     var script = document.createElement('script');
-    script.src = "https://127.0.0.1:8080/build/script.js?" + time;
     script.id = "tmp-script"
     document.body.appendChild(script);
   }
+
+  document.getElementById('tmp-style').href = "https://127.0.0.1:8080/build/style.css?" + time;
+  document.getElementById('tmp-script').src = "https://127.0.0.1:8080/build/script.js?" + time;
 })();
